3b21fc96ce62b3e6f98a0e2d9aed257d6afe0fb6
Rockafella <eugene.kostrikov@gmail.com>
2014-04-20 11:56:23 +0400

    Export all base classes and bundle factory

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = require('./lib');
+var Storage = require('./lib/storage');
+var Memo = require('./lib/memo');
+var Container = require('./lib');
+module.exports = {
+  Container: Container,
+  Memo: Memo,
+  Storage: Storage,
+  bundle: function(prefix, ttl, client, hash){
+    var storage;
+    if (client){
+      storage = new Storage(client, prefix);
+    }else{
+      storage = new Storage(prefix);
+    }
+    return new Memo(storage, ttl, hash);
+  }
+};
